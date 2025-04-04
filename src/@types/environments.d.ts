50beb740ffda6749c3ac87ad733f8c8a9576964c
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-04-04 02:41:06 -0300

    feat: add new envirolments

--- src/@types/environments.d.ts
+++ src/@types/environments.d.ts
@@ -1,6 +1,10 @@
 namespace NodeJS {
   export interface ProcessEnv {
-    PORT: number
-    ORIGIN: string
+    PORT: number;
+    ORIGIN: string;
+    JWT_SECRET: string;
+    JWT_EXPIRES_IN: string;
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRES_IN: string;
   }
 }
