7fb1dd663c4e42d5faf7a6d5c1275b4a0afad46d
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-04-04 02:44:43 -0300

    feat: created test router

---
+++ src/routers/auth/index.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from "express";
+import { authenticate } from "../../middleware/authMiddleware";
+
+const authRoutes = Router();
+
+authRoutes.get("/teste", authenticate, (req: Request, res: Response) => {
+  res.send({
+    message: "Is autenticated",
+    error: {
+      status: false,
+      code: 0,
+    },
+  });
+});
+
+export { authRoutes };
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,16 +1,18 @@
-import { Router } from 'express'
-import type { Request, Response } from 'express'
+import { Router } from "express";
+import type { Request, Response } from "express";
 
-import { signinRouters } from './signin'
+import { signinRouters } from "./signin";
+import { authRoutes } from "./auth";
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   res.status(200).send({
-    message: 'Ok'
-  })
-})
+    message: "Ok",
+  });
+});
 
-router.use('/signin-bff', signinRouters)
+router.use("/signin-bff", signinRouters);
+router.use("/auth", authRoutes);
 
-export { router }
+export { router };
